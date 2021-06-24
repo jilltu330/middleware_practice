34db7414640093d940035d6172134fc257ffc18c
Jill <jilltu330@gmail.com>
2021-06-25 00:16:52 +0800

    feat: install moment & Q1 server req log

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const moment = require('moment')
+
+app.use(function (req, res, next) {
+  console.log(`${moment().format("YYYY-MM-DD HH:mm:ss")} | ${req.method} from ${req.url}`)
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('列出全部 Todo')
 })
@@ -18,6 +25,8 @@ app.get('/:id', (req, res) => {
 app.post('/', (req, res) => {
   res.send('新增一筆  Todo')
 })
+
+
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
